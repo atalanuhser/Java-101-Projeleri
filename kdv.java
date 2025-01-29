7d2945178ff9ea8cf7f468c4eeb16c2dc8f30d33
Nuhser Atala <nuhseratala@gmail.com>
2025-01-29 21:37:40 +0300

    Add files via upload

---
+++ kdv.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args){
+        int armut,elma,domates,muz,patlican;
+        Scanner input= new Scanner(System.in);
+
+
+        System.out.print("Armut kaç kilo ?: ");
+        armut= input.nextInt();
+        System.out.print("Elma kaç kilo ?: ");
+        elma= input.nextInt();
+        System.out.print("Domates kaç kilo ?: ");
+        domates= input.nextInt();
+        System.out.print("Muz kaç kilo ?: ");
+        muz= input.nextInt();
+        System.out.print("Patlıcan kaç kilo ?: ");
+        patlican= input.nextInt();
+
+        double tutar= (armut*2.14)+(elma*3.67)+(domates*1.11)+(muz*0.95)+(patlican*5.0);
+        System.out.print("Toplam Tutar:"+tutar);
+
+    }
+}
