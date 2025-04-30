c1b3866cc4c9dd0c55f0f771efaf3bb81fbb19fd
Nuhser Atala <nuhseratala@gmail.com>
2025-04-30 18:29:38 +0300

    Taksi.java

--- Taksi.java
+++ Taksi.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 public class Taksi {
     public static void main(String[]args){
         int km;
-        double perKm = 2.20, total = 10;
+        double perKm = 2.20, total = 10, minimumucret = 20 ; 
         Scanner input= new Scanner (System.in);
         System.out.println ("Mesafeyi km cinsinden giriniz");
         km = input.nextInt();
+        
         total += (km*perKm);
-        System.out.println("Toplam Tutar:"+total);
+        total = (total < minimumucret) ? minimumucret:total ; 
+        System.out.println("Toplam Tutar:"+ total + "TL");
 
     }
 }
